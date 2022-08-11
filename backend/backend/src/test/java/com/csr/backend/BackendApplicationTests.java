8c5df3969e0296e1fcf7c55804e346025f30441f
abhimanyu <abhimanyu6280@gmail.com>
2022-08-12 02:46:52 +0530

    first commit

---
+++ backend/backend/src/test/java/com/csr/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csr.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
